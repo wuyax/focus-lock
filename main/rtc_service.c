13756d065d4fa9cb9dac638fc9ecba6de4d4ff7e
wuyax <pengqingkai@gmail.com>
2026-06-13 09:32:07 +0800

    feat: add DS3231 RTC service

---
+++ main/rtc_service.c
@@ -0,0 +1,28 @@
+#include "rtc_service.h"
+#include "i2c_manager.h"
+#include "esp_log.h"
+
+static const char *TAG = "rtc";
+static i2c_master_dev_handle_t rtc_dev_handle;
+
+esp_err_t rtc_service_init(void) {
+    i2c_device_config_t dev_cfg = {
+        .dev_addr_7bit = 0x68,
+        .scl_speed_hz = 100000,
+    };
+    return i2c_master_bus_add_device(i2c_bus_handle, &dev_cfg, &rtc_dev_handle);
+}
+
+static uint8_t bcd2dec(uint8_t val) { return ((val / 16 * 10) + (val % 16)); }
+
+esp_err_t rtc_get_time(rtc_time_t *time) {
+    uint8_t data[3];
+    uint8_t reg = 0x00;
+    esp_err_t ret = i2c_master_transmit_receive(rtc_dev_handle, &reg, 1, data, 3, -1);
+    if (ret == ESP_OK) {
+        time->second = bcd2dec(data[0] & 0x7F);
+        time->minute = bcd2dec(data[1]);
+        time->hour = bcd2dec(data[2] & 0x3F);
+    }
+    return ret;
+}
---
+++ main/rtc_service.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <time.h>
+#include "esp_err.h"
+
+typedef struct {
+    int hour;
+    int minute;
+    int second;
+} rtc_time_t;
+
+esp_err_t rtc_service_init(void);
+esp_err_t rtc_get_time(rtc_time_t *time);
